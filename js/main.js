43f14e7f5c6be19e508737cd2811ea8aa0ea9c89
Web-Qwelcer <74631643+Web-Qwelcer@users.noreply.github.com>
2021-05-10 14:29:49 +0300

    JS

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
 let $place_list = document.querySelectorAll('aside ul.places li'),
-    $article_list = document.querySelectorAll('article')
+    $article_list = document.getElementsByClassName('list__article');
 
-let prev_visible_item = 0
+const homeArticle = document.getElementById('home-article');
 
-let $s
+let prev_visible_item = 0;
 
 for (let i = 0; i < $place_list.length; i++)
 {
-    $place_list[i].addEventListener('click', function(){
-        if ($article_list[i+1].style.display != 'block'){
-            $article_list[i+1].style.display = 'block'
-            $article_list[prev_visible_item].style.display = 'none'
-            prev_visible_item = i+1
+    const currentPlace = $place_list[i];
+
+    currentPlace.addEventListener('click', function() {
+        homeArticle.style.display = 'none';
+
+        $place_list[prev_visible_item].classList.remove('active');
+
+        if (currentPlace.classList.contains('active')) {
+            currentPlace.classList.remove('active');
+        } else {
+            currentPlace.classList.add('active');
         }
-    })
+
+        $article_list[prev_visible_item].classList.remove('visible');
+        $article_list[i].classList.add('visible');
+
+        prev_visible_item = i;
+    });
+
+    currentPlace.addEventListener('mouseover', () => {
+        currentPlace.classList.add('hover');
+    }, false);
+
+    currentPlace.addEventListener('mouseleave', () => {
+        currentPlace.classList.remove('hover');
+    }, false);
 }
